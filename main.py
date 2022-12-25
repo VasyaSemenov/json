e983cf8f0a0eb23e8a9e35c8a79619bb2666edd2
dolbognom1 <112643093+dolbognom1@users.noreply.github.com>
2022-12-26 02:57:33 +0500

    1

---
+++ main.py
@@ -0,0 +1,35 @@
+import json
+from pprint import pprint
+from random import randint as rd, choice as ch
+
+
+def write(data, filename):
+    data = json.dumps(data)
+    data = json.loads(str(data))
+    with open(filename, 'w', encoding='utf-8') as file:
+        json.dump(data, file, indent=4)
+
+
+def read(filename):
+    with open(filename, 'r', encoding='utf-8') as file:
+        return json.load(file)
+
+
+class User:
+    def __init__(self):
+        self.name = ch(['Yan', 'Top', 'Alex', 'Jack', 'Cat'])
+        self.age = rd(0, 80)
+        self.id = rd(0, 1000000)
+
+
+data = {
+    'users': []
+}
+
+
+for i in range(100):
+    data['users'].append(User().__dict__)
+
+write(data, 'data.json')
+
+
